07ca6d2a3fb6c2d7ac8e3efae1635e73c912ab8f
AgnidhraGangopadhyay <john.doe@macys.com>
2020-05-19 16:05:07 -0700

    Updating Solution for LC 709 To Lower Case

---
+++ src/Arrays/LC709.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+/*
+709. To Lower Case
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+
+*/
+
+
+public class LC709 {
+    public String toLowerCase(String str) {
+        StringBuilder result = new StringBuilder();
+        for(char letter: str.toCharArray()){
+            if(letter >= 65 && letter <= 90) {
+                result.append((char)(letter + 32));
+            } else {
+                result.append(letter);
+            }
+
+        }
+        return result.toString();
+    }
+}
