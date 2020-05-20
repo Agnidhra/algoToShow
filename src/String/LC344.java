019cd9c000862142e861d9287d09e31081be0a6e
AgnidhraGangopadhyay <john.doe@macys.com>
2020-05-19 19:58:48 -0700

    Uploading solution of LC 344 Reverse String

---
+++ src/String/LC344.java
@@ -0,0 +1,51 @@
+package String;
+
+/*
+344. Reverse String
+Easy
+
+1252
+
+660
+
+Add to List
+
+Share
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+*/
+
+
+public class LC344 {
+    class Solution {
+        public void reverseString(char[] s) {
+            reverse(s, 0, s.length-1);
+        }
+
+        public void reverse(char[] s, int start, int end){
+            if(start >= end){
+                return;
+            } else {
+                char temp = s[start];
+                s[start++] = s[end];
+                s[end--] = temp;
+                reverse(s, start, end);
+            }
+        }
+    }
+}
