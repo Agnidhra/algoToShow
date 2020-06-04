b504df364cb6fcd8dc747425a541599bdd62c961
Agnidhra <agnidhra86@gmail.com>
2020-06-03 20:37:57 -0700

    Updating Comments

--- src/Tree/LC107.java
+++ src/Tree/LC107.java
@@ -4,15 +4,6 @@ import java.util.*;
 
 /*
 107. Binary Tree Level Order Traversal II
-Easy
-
-1215
-
-205
-
-Add to List
-
-Share
 Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
 
 For example:
